e34e324663e238836b2af3d6599cf7871d01ec34
PJH <carpediem.mementomori.1984@gmail.com>
2020-12-09 23:37:14 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let navs = document.querySelectorAll(".nav-content");
+let slides = document.querySelectorAll(".content-slides");
+
+function slideChange() { 
+    // let selectedNav = this.index();
+        
+    // if (selectedNav !== 0) {
+    //     slides[0].style.display = "none";
+    //     slides[selectedNav].style.display = "block";
+    // }
+    console.log(this);
+ 
+}
+
